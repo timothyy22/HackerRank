8b7d11916b8bf1802d81ee934b1b42dfd3ad9c07
timothy <tsingletary1579@gmail.com>
2023-09-18 12:12:47 -0500

    java formatting problem

--- src/DailyHackerRank/HackerRankDay1.java
+++ src/DailyHackerRank/HackerRankDay1.java
@@ -1,5 +1,27 @@
 package DailyHackerRank;
+import java.io.*;
+import java.util.*;
 
 public class HackerRankDay1 {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Header line
+        System.out.println("================================");
+
+        while (scanner.hasNext()) {
+            String s = scanner.next();
+            int x = scanner.nextInt();
+
+            // Format and print the output
+            System.out.printf("%-15s%03d%n", s, x);
+        }
+
+        // Footer line
+        System.out.println("================================");
+
+        scanner.close();
+    }
+    }
 }
